8cef302d72cbcfd0393f68c4f268f0a1ac57f079
Reeyna <reeynarl@gmail.com>
2024-01-16 14:20:42 -0600

    add routes with params

--- 08-Express/app.js
+++ 08-Express/app.js
@@ -12,7 +12,33 @@ app.get('/cursos', (req, res) => {
 })
 
 app.get('/cursos/programacion', (req, res) => {
-  res.send(infoCursos.programacion)
+  res.send(JSON.stringify(infoCursos.programacion))
+})
+
+app.get('/cursos/matematicas', (req, res) => {
+  res.send(JSON.stringify(infoCursos.matematicas))
+})
+
+//
+app.get('/cursos/programacion/:language', (req, res) => {
+  // console.log(req.params.language) // { language: 'SQL' }
+  const language = req.params.language
+  const result = infoCursos.programacion.filter(course => course.language === language)
+  if(result.length === 0){
+    res.status(404).send('No se encontraron los registros')
+  }
+
+  res.send(JSON.stringify(result))
+})
+
+//ruta que filtra las materias de matematicas por nivel
+app.get('/cursos/matematicas/:level', (req, res) => {
+  const level = req.params.level
+  const result = infoCursos.matematicas.filter(course => course.level === level)
+  if(result.length === 0){
+    res.status(404).send('No se encontraron los registros')
+  }
+  res.send(JSON.stringify(result))
 })
 
 const PORT = process.env.PORT || 3000
